34da9fe77462932145b5c5e1052fabd2512614ee
evgenyzhupanik <zhenya1975@mail.ru>
2022-06-09 22:37:59 +0300

    все работает

--- routes/routes.py
+++ routes/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, abort
+from flask import Blueprint, render_template, redirect, url_for, abort, request, jsonify
 from models.models import ParticipantsDB, FightsDB, CompetitionsDB, BacklogDB, RegistrationsDB
 from extensions import extensions
 from sqlalchemy import desc
@@ -177,9 +177,152 @@ def competition_view(competition_id):
 
     return render_template("competition.html", fight_data=last_created_fight)
 
+
 @home.route('/competition_delete/')
 def competition_delete():
     db.session.query(CompetitionsDB).delete()
     db.session.commit()
 
     return "deleted"
+
+
+@home.route('/ajaxfile', methods=["POST", "GET"])
+def ajaxfile():
+    final_status = "continue"
+    if request.method == 'POST':
+        fight_id = request.form['fight_id']
+        winner_color = request.form['winner_color']
+        current_fight_data = FightsDB.query.get(fight_id)
+        competition_id = current_fight_data.competition_id
+        current_round_number = current_fight_data.round_number
+
+        # обновляем данные о победителе и проигравшем
+        if winner_color == "red":
+            current_fight_data.blue_fighter.activity_status = 0
+            current_fight_data.fight_winner_id = current_fight_data.red_fighter_id
+        else:
+            current_fight_data.red_fighter.activity_status = 0
+            current_fight_data.fight_winner_id = current_fight_data.blue_fighter_id
+        try:
+            db.session.commit()
+        except Exception as e:
+            print("Не удалось вывести из игры синего бойца", e)
+            db.session.rollback()
+        # добавляем в бэклог в следующий круг новую запись с победившим
+        if winner_color == "red":
+            new_backlog_record = BacklogDB(reg_id=current_fight_data.red_fighter_id, competition_id=competition_id,
+                                           round_number=current_round_number + 1)
+        else:
+            new_backlog_record = BacklogDB(reg_id=current_fight_data.blue_fighter_id, competition_id=competition_id,
+                                           round_number=current_round_number + 1)
+        db.session.add(new_backlog_record)
+        try:
+            db.session.commit()
+
+        except Exception as e:
+            print("не удалось добавить запись с победившим в бэклог", e)
+            db.session.rollback()
+
+        # проверяем ситуацию в бэклоге в текущем и следующем раунде
+        next_round_backlog_data = BacklogDB.query.filter_by(competition_id=competition_id,
+                                                            round_number=current_round_number + 1).all()
+
+        current_backlog_data = BacklogDB.query.filter_by(competition_id=competition_id,
+                                                         round_number=current_round_number).all()
+
+        # print("len(current_backlog_data): ", len(current_backlog_data), "len(next_round_backlog_data): ", len(next_round_backlog_data))
+        if len(current_backlog_data) == 1 and len(next_round_backlog_data) == 1:
+
+            final_status = 'continue'
+            next_round_fighter_data = BacklogDB.query.filter_by(competition_id=competition_id,
+                                                                round_number=current_round_number + 1).first()
+
+            current_round_fighter_data = BacklogDB.query.filter_by(competition_id=competition_id,
+                                                                   round_number=current_round_number).first()
+
+            if next_round_fighter_data.fighter_id != current_round_fighter_data.fighter_id:
+                current_round_fighter_data.round_number = current_round_number + 1
+                current_round_number = current_round_number + 1
+                try:
+                    db.session.commit()
+
+                except Exception as e:
+                    print("не удалось переписать номер круга в записи бойца из следующего круга", e)
+                    db.session.rollback()
+
+                fight_create_func(competition_id, current_round_number, final_status)
+                # удаляем из бэклога записи с бойцами
+                delete_backlog_records(competition_id, current_round_number)
+
+        elif len(current_backlog_data) == 0 and len(next_round_backlog_data) == 1:
+            final_status = 'finish'
+
+            clear_backlog(competition_id)
+
+            return jsonify(
+                {
+                    'final_status': final_status,
+                    'fight_id': fight_id,
+                })
+
+        elif len(current_backlog_data) == 0 and len(next_round_backlog_data) == 0:
+            final_status = 'finish'
+
+            return jsonify(
+                {
+                    'final_status': final_status,
+                    'fight_id': fight_id,
+                })
+
+        # если в след раунде два бойца, а в текущем ни одного
+        elif len(current_backlog_data) == 0 and len(next_round_backlog_data) > 1:
+            current_round_number = current_round_number + 1
+            final_status = 'continue'
+            fight_create_func(competition_id, current_round_number, final_status)
+            delete_backlog_records(competition_id, current_round_number)
+
+        elif len(current_backlog_data) == 1 and len(next_round_backlog_data) > 1:
+            current_round_fighter_data = BacklogDB.query.filter_by(competition_id=competition_id,
+                                                                   round_number=current_round_number).first()
+            current_round_fighter_data.round_number = current_round_number + 1
+            try:
+                db.session.commit()
+            except Exception as e:
+                print("не удалось перекинуть бойца в бэклог следующего круга", e)
+                db.session.rollback()
+            current_round_number = current_round_number + 1
+            final_status = 'continue'
+            fight_create_func(competition_id, current_round_number, final_status)
+            # удаляем из бэклога записи с бойцами
+            delete_backlog_records(competition_id, current_round_number)
+
+        elif len(current_backlog_data) > 1:
+            print("len(current_backlog_data) > 1")
+            fight_create_func(competition_id, current_round_number, 'continue')
+            delete_backlog_records(competition_id, current_round_number)
+        else:
+            print("что-то непонятное")
+
+        last_created_fight = FightsDB.query.filter_by(competition_id=competition_id,
+                                                      round_number=current_round_number).order_by(
+            desc(FightsDB.fight_id)).first()
+
+        return jsonify(
+            {'htmlresponsered': render_template('response_red_fighter_div.html', fight_data=last_created_fight),
+             'htmlresponseblue': render_template('response_blue_fighter_div.html', fight_data=last_created_fight),
+             'htmlresponsetitle': render_template('response_title_div.html', fight_data=last_created_fight),
+             'final_status': final_status,
+             'fight_id': last_created_fight.fight_id
+             })
+
+
+@home.route('/competition_finish/<int:fight_id>')
+def finish(fight_id):
+    fight_data = FightsDB.query.get(fight_id)
+    winner_reg_id = fight_data.fight_winner_id
+    competition_id = fight_data.competition_id
+    registration_data = RegistrationsDB.query.get(winner_reg_id)
+    participant_id = registration_data.participant_id
+    winner_data = ParticipantsDB.query.get(participant_id)
+    fights_data = FightsDB.query.filter_by(competition_id=competition_id).all()
+    return render_template("finish.html", winner_data=winner_data, fights_data=fights_data)
