366f08e42973b8231135d4174980f4458dc79bdf
evgenyzhupanik <zhenya1975@mail.ru>
2022-06-09 22:28:42 +0300

    первый экран работает

--- routes/routes.py
+++ routes/routes.py
@@ -12,10 +12,10 @@ def fight_create_func(competition_id, round_number, final_status):
     round_number = round_number
     final_status = final_status
     backlog_data = BacklogDB.query.filter_by(competition_id=competition_id, round_number=round_number).all()
-    red_fighter_id = backlog_data[0].fighter_id
-    blue_fighter_id = backlog_data[1].fighter_id
-    new_fight = FightsDB(competition_id=competition_id, round_number=round_number, red_fighter_id=red_fighter_id,
-                         blue_fighter_id=blue_fighter_id, final_status=final_status)
+    red_fighter_reg_id = backlog_data[0].reg_id
+    blue_fighter_reg_id = backlog_data[1].reg_id
+    new_fight = FightsDB(competition_id=competition_id, round_number=round_number, red_fighter_id=red_fighter_reg_id,
+                         blue_fighter_id=blue_fighter_reg_id, final_status=final_status)
     db.session.add(new_fight)
 
     try:
@@ -38,15 +38,15 @@ def delete_backlog_records(competition_id, round_number):
         desc(FightsDB.fight_id)).first()
     # удаляем записи из бэклога бойцов, которые зашли в бой
     backlog_record_to_delete_red = BacklogDB.query.filter_by(competition_id=competition_id, round_number=round_number,
-                                                             fighter_id=last_created_fight.red_fighter_id).order_by(
-        desc(BacklogDB.fighter_id)).first()
+                                                             reg_id=last_created_fight.red_fighter_id).order_by(
+        desc(BacklogDB.reg_id)).first()
     if backlog_record_to_delete_red is None:
         abort(404, description="No backlog record was Found with the given ID")
     else:
         db.session.delete(backlog_record_to_delete_red)
     backlog_record_to_delete_blue = BacklogDB.query.filter_by(competition_id=competition_id, round_number=round_number,
-                                                              fighter_id=last_created_fight.blue_fighter_id).order_by(
-        desc(BacklogDB.fighter_id)).first()
+                                                              reg_id=last_created_fight.blue_fighter_id).order_by(
+        desc(BacklogDB.reg_id)).first()
     if backlog_record_to_delete_blue is None:
         abort(404, description="No backlog record was Found with the given ID")
     else:
@@ -118,8 +118,64 @@ def competition_create_new():
             print("Не удалось создать запись в бэклоге", e)
             db.session.rollback()
 
-    return "hello"
+    # проверяем ситуацию в бэклоге в текущем и следующем раунде
+    current_backlog_data = BacklogDB.query.filter_by(competition_id=competition_id, round_number=1).all()
+    next_round_backlog_data = BacklogDB.query.filter_by(competition_id=competition_id, round_number=2).all()
 
+    if len(current_backlog_data) == 0 and len(next_round_backlog_data) == 2:
+        current_round_number = 2
+
+        final_status = 'continue'
+        fight_create_func(competition_id, current_round_number, final_status)
+        # удаляем из бэклога записи с бойцами
+        delete_backlog_records(competition_id, current_round_number)
+    elif len(current_backlog_data) == 2 and len(next_round_backlog_data) == 0:
+
+        current_round_number = 1
+        final_status = 'continue'
+        fight_create_func(competition_id, current_round_number, final_status)
+        # удаляем из бэклога записи с бойцами
+        delete_backlog_records(competition_id, current_round_number)
+    elif len(current_backlog_data) == 1 and len(next_round_backlog_data) == 1:
+
+        current_round_number = 1
+        final_status = 'continue'
+        next_round_fighter_data = BacklogDB.query.filter_by(competition_id=competition_id,
+                                                            round_number=current_round_number + 1).first()
+        # проверяем на случай если это один и тот же боец
+        current_round_fighter_data = BacklogDB.query.filter_by(competition_id=competition_id,
+                                                               round_number=current_round_number).first()
+        if next_round_fighter_data.fighter_id != current_round_fighter_data.fighter_id:
+            next_round_fighter_data.round_number = current_round_number
+            try:
+                db.session.commit()
+
+            except Exception as e:
+                print("не удалось переписать номер круга в записи бойца из следующего круга", e)
+                db.session.rollback()
+
+        fight_create_func(competition_id, current_round_number, final_status)
+        # удаляем из бэклога записи с бойцами
+        delete_backlog_records(competition_id, current_round_number)
+    else:
+        fight_create_func(competition_id, 1, 'continue')
+        # удаляем из бэклога записи с созданным боем
+        last_created_fight = FightsDB.query.filter_by(competition_id=competition_id).order_by(
+            desc(FightsDB.fight_id)).first()
+        # удаляем записи из бэклога бойцов, которые зашли в бой
+        delete_backlog_records(last_created_fight.competition_id, 1)
+
+    return redirect(url_for('home.competition_view', competition_id=competition_id))
+
+
+@home.route('/competition/<int:competition_id>')
+def competition_view(competition_id):
+    competition_id = competition_id
+    # round_number = fight_create_func(round_number_prev)
+    last_created_fight = FightsDB.query.filter_by(competition_id=competition_id).order_by(
+        desc(FightsDB.fight_id)).first()
+
+    return render_template("competition.html", fight_data=last_created_fight)
 
 @home.route('/competition_delete/')
 def competition_delete():
